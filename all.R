5007c3d16d70b2c1881ba5778b3ded3ec9f5a2ea
Nadya M <39595944+nadyaeiou@users.noreply.github.com>
2020-10-27 04:50:43 +0800

    Create all.R

---
+++ all.R
@@ -0,0 +1,15 @@
+library(dplyr)
+
+
+
+round5 <- function(thing_to_round) {return(round(thing_to_round, 5))}
+
+
+
+sigstars <- function(pval) {
+  stars <- ifelse(
+    pval < .001, "***", ifelse(
+      pval < .01, "**", ifelse(
+        pval < .05, "*", "")))
+  return(stars)
+}
