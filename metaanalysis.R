f50879a182766d2c1550701758e9a94cd94354a3
Nadya M <39595944+nadyaeiou@users.noreply.github.com>
2020-10-27 13:13:19 +0800

    Update metaanalysis.R

--- metaanalysis.R
+++ metaanalysis.R
@@ -21,7 +21,33 @@ funnel_and_ranktest <- function(metafor.output, legend = TRUE, xlab = "Effect si
 
 
 
-summary_and_forest <- function(metafor.output, xlab = "Effect size", specific_label = "") {
+rma.mv_table <- function(rma.mv.output, special_rowname = FALSE) {
+
+  # count how many estimates (intercept and slopes) have been computed
+  # if more than 1 (i.e., there are slopes) STOP the function
+  if(length(rma.mv.output$b) > 1) {
+    stop("This function is meant for intercept-only models. For models with slopes (moderators), please use summary() to ensure you get full information.")
+  }
+  
+  # conduct data extraction
+  res <- data.frame(
+    n = rma.mv.output[["s.nlevels"]][1], k = rma.mv.output[["s.nlevels"]][2],
+    effectsize = round5(rma.mv.output$b), se = round5(rma.mv.output$se),
+    zval = round5(rma.mv.output$zval), pval = round5(rma.mv.output$pval),
+    sig = sigstars(rma.mv.output$pval),
+    ci.lb = round5(rma.mv.output$ci.lb), ci.ub = round5(rma.mv.output$ci.ub))
+  
+  # set rowname
+  if(special_rowname == FALSE){rownames(res) <- NULL}
+  else{rownames(res) <- special_rowname}
+
+  # return formatted row
+  return(res)
+}
+
+
+
+summary_and_forest <- function(metafor.output, xlab = "Effect size", specific_label = "", compressed_summary = FALSE) {
 
   mlab = "Overall effect size"
   if(specific_label != "") {mlab = paste0("Overall effect size for ", specific_label)}
@@ -29,50 +55,6 @@ summary_and_forest <- function(metafor.output, xlab = "Effect size", specific_la
   forest(metafor.output, top = 1, xlab = xlab,
          mlab = mlab)
 
-  summary(metafor.output)
-}
-
-
-
-rma.mv_table <- function(rma.mv.output, special_rowname = FALSE, debug = FALSE) {
-
-  # count how many estimates (intercept and slopes) have been computed
-  estimate_counts <- length(rma.mv.output$b)
-  if(debug) {print(estimate_counts)}
-  
-  # initialise empty dataframe to collect each row
-  res_compiled <- data.frame()
-  
-  # loop through each estimate
-  for(i in 1:estimate_counts) {
-    
-    # store extracted values in single-row dataframe
-    res_row <- data.frame(
-      n = rma.mv.output[["s.nlevels"]][1], k = rma.mv.output[["s.nlevels"]][2],
-      estimate = round5(rma.mv.output$b[i]), se = round5(rma.mv.output$se[i]),
-      zval = round5(rma.mv.output$zval[i]), pval = round5(rma.mv.output$pval[i]),
-      sig = sigstars(rma.mv.output$pval[i]),
-      ci.lb = round5(rma.mv.output$ci.lb[i]), ci.ub = round5(rma.mv.output$ci.ub[i]))
-    
-    # if debugger enabled, print this row
-    if(debug) {print(res_row)}
-    
-    # bind with master dataframe
-    res_compiled <- rbind(res_compiled, res_row)
-    
-  }
-  
-  # set rownames
-  if(special_rowname == FALSE) {rownames(res_compiled) <- NULL}
-  else{
-    if(estimate_counts == 1) {rownames(res_compiled) <- special_rowname}
-    else{
-      list_of_estimates <- c("intercept", rma.mv.output[["call"]][["mods"]])
-      special_rowname_stems <- c(rep(special_rowname, estimate_counts))
-      special_rowname_compiled <- paste0(special_rowname_stems, list_of_estimates)
-      rownames(res_compiled) <- special_rowname_compiled
-    }
-  }
-
-  return(res_compiled)
+  if(compressed_summary) {rma.mv_table(metafor.output)}
+  else {summary(metafor.output)}
 }
