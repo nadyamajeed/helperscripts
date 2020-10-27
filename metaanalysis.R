0d364ff9be3b5cd76ad8baecd24456081b502e06
Nadya M <39595944+nadyaeiou@users.noreply.github.com>
2020-10-27 12:41:21 +0800

    Update metaanalysis.R

--- metaanalysis.R
+++ metaanalysis.R
@@ -36,15 +36,39 @@ summary_and_forest <- function(metafor.output, xlab = "Effect size", specific_la
 
 rma.mv_table <- function(rma.mv.output, special_rowname = FALSE) {
 
-  res <- data.frame(
-    n = rma.mv.output[["s.nlevels"]][1], k = rma.mv.output[["s.nlevels"]][2],
-    effectsize = round5(rma.mv.output$b), se = round5(rma.mv.output$se),
-    zval = round5(rma.mv.output$zval), pval = round5(rma.mv.output$pval),
-    sig = sigstars(rma.mv.output$pval),
-    ci.lb = round5(rma.mv.output$ci.lb), ci.ub = round5(rma.mv.output$ci.ub))
+  # count how many estimates (intercept and slopes) have been computed
+  estimate_counts <- length(rma.mv.output$b)
   
-  if(special_rowname == FALSE){rownames(res) <- NULL}
-  else{rownames(res) <- special_rowname}
+  # initialise empty dataframe to collect each row
+  res_compiled <- data.frame()
+  
+  # loop through each estimate
+  for(i in estimate_counts) {
+    
+    # store extracted values in single-row dataframe
+    res_row <- data.frame(
+      n = rma.mv.output[["s.nlevels"]][1], k = rma.mv.output[["s.nlevels"]][2],
+      estimate = round5(rma.mv.output$b[i]), se = round5(rma.mv.output$se[i]),
+      zval = round5(rma.mv.output$zval[i]), pval = round5(rma.mv.output$pval[i]),
+      sig = sigstars(rma.mv.output$pval[i]),
+      ci.lb = round5(rma.mv.output$ci.lb[i]), ci.ub = round5(rma.mv.output$ci.ub[i]))
+    
+    # bind with master dataframe
+    rbind(res_compiled, res_row)
+    
+  }
+  
+  # set rownames
+  if(special_rowname == FALSE) {rownames(res_compiled) <- NULL}
+  else{
+    if(estimate_counts == 1) {rownames(res_compiled) <- special_rowname}
+    else{
+      list_of_estimates <- c("intercept", rma.mv.output[["call"]][["mods"]])
+      special_rowname_stems <- c(rep(special_rowname, estimate_counts))
+      special_rowname_compiled <- paste0(special_rowname_stems, list_of_estimates)
+      rownames(res_compiled) <- special_rowname_compiled
+    }
+  }
 
   return(res)
 }
