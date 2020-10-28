072fe6dc96379a5965eafea2cbf4e23d24d55746
Nadya M <39595944+nadyaeiou@users.noreply.github.com>
2020-10-28 23:44:44 +0800

    Update latent.R

--- latent.R
+++ latent.R
@@ -18,35 +18,38 @@ library(lavaan); library(lavaanPlot); library(blavaan); library(tibble)
 
 
 
-n.lavaan <- function(
-  model_in, cfa_or_sem, data, show_output = TRUE,
-  bayesian = FALSE, bayesian_cores = 1, bayesian_n.chains = 5, bayesian_burnin = 1000, bayesian_sample = 5000
-  ) {
+n.lavaan <- function(model_in, cfa_or_sem, data, show_output = TRUE) {
   if(!is.character(model_in)) {stop("\nPass in a model that lavaan can read.\n")}
   
-  if(!bayesian) {
-    if(cfa_or_sem=="cfa") {out <- model_in %>% cfa(data = data, missing = "fiml", mimic = "Mplus")}
-    if(cfa_or_sem=="sem") {out <- model_in %>% sem(data = data, missing = "fiml", mimic = "Mplus")}
-    
-    lavaanPlot(
-      model = out,
-      node_options = list(shape = "box", fontname = "Helvetica"),
-      edge_options = list(color = "grey"), coefs = TRUE, covs = TRUE, stand = TRUE, stars = TRUE)
-  }
+  if(cfa_or_sem=="cfa") {out <- model_in %>% cfa(data = data, missing = "fiml", mimic = "Mplus")}
+  if(cfa_or_sem=="sem") {out <- model_in %>% sem(data = data, missing = "fiml", mimic = "Mplus")}
   
-  if(bayesian){
-    if(cfa_or_sem=="cfa") {stop(cat("\nBayesian CFA not set yet.\n"))}
-    if(cfa_or_sem=="sem"){
-      out <-
-        model_in %>% bsem(
-          data = data, mimic = "Mplus",
-          n.chains = bayesian_n.chains, burnin = bayesian_burnin, sample = bayesian_sample,
-          bcontrol = list(cores = bayesian_cores)     # 'cores' allows multicore use, faster
-        )}
-  }
+  lavaanPlot(
+    model = out,
+    node_options = list(shape = "box", fontname = "Helvetica"),
+    edge_options = list(color = "grey"), coefs = TRUE, covs = TRUE, stand = TRUE, stars = TRUE) %>%
+    print()
   
-  if(show_output){summary(out, standardized = TRUE, fit.measures = TRUE)}
+  if(show_output) {summary(out, standardized = TRUE, fit.measures = TRUE)}
+  return(out)
+}
+
+
+
+n.bsem <- function(
+  model_in, data, show_output = TRUE,
+  bayesian_cores = 1, bayesian_n.chains = 5, bayesian_burnin = 1000, bayesian_sample = 5000
+) {
+  if(!is.character(model_in)) {stop("\nPass in a model that lavaan can read.\n")}
   
+  out <-
+    model_in %>% bsem(
+      data = data, mimic = "Mplus",
+      n.chains = bayesian_n.chains, burnin = bayesian_burnin, sample = bayesian_sample,
+      bcontrol = list(cores = bayesian_cores)     # 'cores' allows multicore use, faster
+    )
+  
+  if(show_output) {summary(out, standardized = TRUE, fit.measures = TRUE)}
   return(out)
 }
 
