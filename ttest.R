952e1e125d2acd068cd98684aec2046ce107bfeb
Nadyanna Majeed <39595944+nadyamajeed@users.noreply.github.com>
2021-05-23 09:54:03 +0800

    Update ttest.R
    Changed output of ttestFNB() to table to allow cleaner output for later combination.

--- ttest.R
+++ ttest.R
@@ -4,18 +4,18 @@ devtools::source_url("https://raw.githubusercontent.com/nadyamajeed/helperscript
 
 cat("\n####################")
 cat("\nLoading Nadya's t-test upgrades (with Bayesian t-test!) from Github.")
-cat("\n            Version : 0.0.1.9000")
-cat("\n        Last update : 23 May 2021, 9:30am")
-cat("\nRequired Package(s) : BayesFactor")
+cat("\n            Version : 0.0.2.9000")
+cat("\n        Last update : 23 May 2021, 9:52am")
+cat("\nRequired Package(s) : BayesFactor, effectsize")
 cat("\n")
 
-starttime <- Sys.time()
+starttime = Sys.time()
 
 ##########
 
 
 
-intext_t <- function(t.test.output) {
+intext_t = function(t.test.output) {
   if(class(t.test.output) != "htest") stop("Needs output from t.test() function.")
   degrees_of_freedom = t.test.output$parameter
   t_statistic = t.test.output$statistic
@@ -25,7 +25,7 @@ intext_t <- function(t.test.output) {
 
 
 
-tBF10 <- function(ttestBF.output) {
+tBF10 = function(ttestBF.output) {
   BF10 = ttestBF.output@bayesFactor[["bf"]]
   if(BF10 < 0.01) {evcat = "Decisive evidence for H0"}
   else if(BF10 < 0.03) {evcat = "Very strong evidence for H0"}
@@ -43,7 +43,7 @@ tBF10 <- function(ttestBF.output) {
 
 
 
-intext_t_bayes <- function(ttestBF.output) {
+intext_t_bayes = function(ttestBF.output) {
   out = tBF10(ttestBF.output)
   BF10 = out$BF10 %>% round2(force = TRUE)
   return(paste0("BF10 = ", BF10, ", ", out$evcat, sep = ""))
@@ -55,7 +55,8 @@ ttestFNB = function(
   col1 = NULL, col2 = NULL,
   formula = NULL, data = NULL,
   paired = NULL, var.equal = TRUE,
-  d = TRUE) {
+  d = TRUE,
+  print = TRUE, viewtable = FALSE) {
   
   if(is.null(col1) & is.null(col2) & is.null(formula) & is.null(data)) (stop("Incorrect input format. Try again."))
   
@@ -69,23 +70,43 @@ ttestFNB = function(
     col2 = data[iv_values == iv_levels[2], dv_name] %>% unlist()
   }
   
-  t.test(x = col1, y = col2, paired = paired, var.equal = var.equal) %>% intext_t() %>% cat(); cat("\n")
-  BayesFactor::ttestBF(x = col1, y = col2, paired = paired) %>% intext_t_bayes() %>% cat(); cat("\n")
+  res.freq = t.test(x = col1, y = col2, paired = paired, var.equal = var.equal)
+  res.freq %>% intext_t() %>% cat(); cat("\n")
   
-  cat("\n")
-  cat("Groups:", iv_levels); cat("\n")
-  cat("Mean in first group :", mean(col1, na.rm = TRUE) %>% round(2)); cat("\n")
-  cat("Mean in second group:", mean(col2, na.rm = TRUE) %>% round(2)); cat("\n")
-  print(effectsize::cohens_d(x = col1, y = col2, paired = paired))
+  res.bayes = BayesFactor::ttestBF(x = col1, y = col2, paired = paired)
+  res.bayes %>% intext_t_bayes() %>% cat(); cat("\n")
   
-  invisible(NULL)
+  cohensd = effectsize::cohens_d(x = col1, y = col2, paired = paired)
+  
+  table_out = data.frame(
+    't' = res.freq$statistic %>% round(2),
+    'df' = res.freq$parameter,
+    'p' = res.freq$p.value %>% round(3),
+    'BF10' = res.bayes@bayesFactor[["bf"]] %>% round(2),
+    m1 = mean(col1, na.rm = TRUE) %>% round(2),
+    m2 = mean(col2, na.rm = TRUE) %>% round(2),
+    'd' = cohensd$Cohens_d %>% round(2),
+    'd_CILB' = cohensd$CI_low %>% round(2),
+    'd_CIUB' = cohensd$CI_high %>% round(2)
+  )
+  
+  if(print) {
+    cat("\n")
+    cat("Groups:", iv_levels, "\n")
+    cat("Mean in first group :", table_out$m1, "\n")
+    cat("Mean in second group:", table_out$m2, "\n")
+    print(cohensd)
+  }
+  if(viewtable) {View(table_out)}
+
+  return(invisible(table_out))
 }
 
 
 
 ##########
 
-endtime <- Sys.time()
+endtime = Sys.time()
 cat("\nFinished loading Nadya's t-test upgrades.")
 cat("\nTime taken :", (endtime - starttime))
 cat("\nUse ttestFNB() to run both frequentist and Bayesian t-tests at the same time :D")
