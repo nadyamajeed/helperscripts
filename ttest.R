a5d65db9e7021e55e6f208fe249ad256b47c111d
Nadyanna Majeed <39595944+nadyamajeed@users.noreply.github.com>
2021-06-28 00:19:41 +0800

    Update ttest.R
    (1) IMPORTANT fix -- BF10s in old version were actually logBF10s.
    (2) Now using effectsize::interpret_bf with default jeffreys1961 rule to get evidence categories for BF10.
    (3) Also added dp argument (default dp = 2) to ttestFNB to allow more than 2 decimal places.

--- ttest.R
+++ ttest.R
@@ -4,9 +4,9 @@ devtools::source_url("https://raw.githubusercontent.com/nadyamajeed/helperscript
 
 cat("\n####################")
 cat("\nLoading Nadya's t-test upgrades (with Bayesian t-test!) from Github.")
-cat("\n            Version : 0.0.2.9000")
-cat("\n        Last update : 23 May 2021, 9:52am")
-cat("\nRequired Package(s) : BayesFactor, effectsize")
+cat("\n            Version : 0.0.3.9000")
+cat("\n        Last update : 27 Jun 2021, 11:14pm")
+cat("\nRequired Package(s) : BayesFactor (0.9.12-4.2), effectsize (0.4.5)")
 cat("\n")
 
 starttime = Sys.time()
@@ -26,18 +26,12 @@ intext_t = function(t.test.output) {
 
 
 tBF10 = function(ttestBF.output) {
-  BF10 = ttestBF.output@bayesFactor[["bf"]]
-  if(BF10 < 0.01) {evcat = "Decisive evidence for H0"}
-  else if(BF10 < 0.03) {evcat = "Very strong evidence for H0"}
-  else if(BF10 < 0.10)  {evcat = "Strong evidence for H0"}
-  else if(BF10 < 0.33)  {evcat = "Substantial evidence for H0"}
-  else if(BF10 < 1.00)  {evcat = "Anecdotal evidence for H0"}
-  else if(BF10 == 1)  {evcat = "No evidence"}
-  else if(BF10 < 3)  {evcat = "Anecdotal evidence for H1"}
-  else if(BF10 < 10)  {evcat = "Substantial evidence for H1"}
-  else if(BF10 < 30)  {evcat = "Strong evidence for H1"}
-  else if(BF10 < 100)  {evcat = "Very strong evidence for H1"}
-  else if(BF10 >= 100)  {evcat = "Decisive evidence for H1"}
+  # note that ttestBF.output@bayesFactor[["bf"]] gives logBF10 as of BayesFactor version 0.9.12-4.2
+  # need to exp() to get BF10
+  BF10 = exp(ttestBF.output@bayesFactor[["bf"]])
+  # use effectsize::interpret_bf() to get evidence category
+  evcat = paste0(effectsize::interpret_bf(BF10, include_value = FALSE, exact = TRUE), " H1 (rule: jeffreys1961)")
+  # return BF10 and evidence category
   return(list(BF10 = BF10, evcat = evcat))
 }
 
@@ -46,7 +40,7 @@ tBF10 = function(ttestBF.output) {
 intext_t_bayes = function(ttestBF.output) {
   out = tBF10(ttestBF.output)
   BF10 = out$BF10 %>% round2(force = TRUE)
-  return(paste0("BF10 = ", BF10, ", ", out$evcat, sep = ""))
+  return(paste0("BF10 = ", BF10, ", ", out$evcat))
 }
 
 
@@ -56,6 +50,7 @@ ttestFNB = function(
   formula = NULL, data = NULL,
   paired = NULL, var.equal = TRUE,
   d = TRUE,
+  dp = 2,
   print = TRUE, viewtable = FALSE) {
   
   if(is.null(col1) & is.null(col2) & is.null(formula) & is.null(data)) (stop("Incorrect input format. Try again."))
@@ -79,26 +74,29 @@ ttestFNB = function(
   cohensd = effectsize::cohens_d(x = col1, y = col2, paired = paired)
   
   table_out = data.frame(
-    't' = res.freq$statistic %>% round(2),
+    't' = res.freq$statistic %>% round(dp),
     'df' = res.freq$parameter,
     'p' = res.freq$p.value %>% round(3),
-    'BF10' = res.bayes@bayesFactor[["bf"]] %>% round(2),
-    m1 = mean(col1, na.rm = TRUE) %>% round(2),
-    m2 = mean(col2, na.rm = TRUE) %>% round(2),
-    'd' = cohensd$Cohens_d %>% round(2),
-    'd_CILB' = cohensd$CI_low %>% round(2),
-    'd_CIUB' = cohensd$CI_high %>% round(2)
+    'BF10' = tBF10(res.bayes)$BF10 %>% round(dp),
+    'BF_cat' = tBF10(res.bayes)$evcat,
+    m1 = mean(col1, na.rm = TRUE) %>% round(dp),
+    sd1 = sd(col1, na.rm = TRUE) %>% round(dp),
+    m2 = mean(col2, na.rm = TRUE) %>% round(dp),
+    sd2 = sd(col2, na.rm = TRUE) %>% round(dp),
+    'd' = cohensd$Cohens_d %>% round(dp),
+    'd_CILB' = cohensd$CI_low %>% round(dp),
+    'd_CIUB' = cohensd$CI_high %>% round(dp)
   )
   
   if(print) {
     cat("\n")
     cat("Groups:", iv_levels, "\n")
-    cat("Mean in first group :", table_out$m1, "\n")
-    cat("Mean in second group:", table_out$m2, "\n")
+    cat("M (SD) in first group : ", table_out$m1, " (", table_out$sd1, ")\n", sep = "")
+    cat("M (SD) in second group: ", table_out$m2, " (", table_out$sd2, ")\n", sep = "")
     print(cohensd)
   }
   if(viewtable) {View(table_out)}
-
+  
   return(invisible(table_out))
 }
 
