286ac17d359f334a97fb9f0f8a313216627eb602
Nadyanna Majeed <39595944+nadyamajeed@users.noreply.github.com>
2022-04-14 17:29:05 +0800

    Update metaanalysis.R

--- metaanalysis.R
+++ metaanalysis.R
@@ -4,10 +4,11 @@ devtools::source_url("https://raw.githubusercontent.com/nadyamajeed/helperscript
 
 cat("\n####################")
 cat("\nLoading Nadya's meta-analysis upgrades from Github.")
-cat("\nPackage(s): metafor")
+cat("\Version    : 14 Apr 2022, 5:30 PM")
+cat("\nPackage(s): metafor (written for 3.0-2)")
 cat("\n")
 
-starttime <- Sys.time()
+starttime = Sys.time()
 
 ##########
 
@@ -17,21 +18,21 @@ library(metafor)
 
 
 
-funnel_and_ranktest <- function(metafor.output, legend = TRUE, xlab = "Effect size") {
-
+funnel_and_ranktest = function(metafor.output, legend = TRUE, xlab = "Effect size") {
+  
   # decrease margins so the full space is used
   par(mar=c(5,4,1,2))
-
+  
   # draw funnel plot
   funnel(metafor.output, legend = legend, xlab = xlab)
-
+  
   # rank correlation test, kendall's tau
   ranktest(metafor.output)
 }
 
 
 
-rma.mv_table <- function(rma.mv.output, special_rowname = "", continuous = FALSE) {
+rma.mv_table = function(rma.mv.output, special_rowname = "", continuous = FALSE) {
   
   # count how many estimates (intercept and slopes) have been computed
   # if more than 1 (i.e., there are slopes) and continuous == FALSE, STOP the function
@@ -41,14 +42,27 @@ rma.mv_table <- function(rma.mv.output, special_rowname = "", continuous = FALSE
   
   # for intercept-only models (no mods)
   if(!continuous) {
-    # conduct data extraction
-    res <- data.frame(
-      'n' = rma.mv.output[["s.nlevels"]][1], 'k' = rma.mv.output[["s.nlevels"]][2],
+    # conduct main data extraction
+    res = data.frame(
       'effectsize' = round5(rma.mv.output$b), 'se' = round5(rma.mv.output$se),
       'zval' = round5(rma.mv.output$zval), 'pval' = round3(rma.mv.output$pval, force = TRUE),
       'sig' = sigstars(rma.mv.output$pval),
       'ci.lb' = round5(rma.mv.output$ci.lb), 'ci.ub' = round5(rma.mv.output$ci.ub)
-      )
+    )
+    # find out how many levels of nesting
+    nestLevels = length(rma.mv.output[["s.nlevels"]])
+    # extract n+k or n+m+k respectively
+    if(nestLevels == 2) {res = res %>%
+      dplyr::mutate(
+        'n' = rma.mv.output[["s.nlevels"]][1],
+        'k' = rma.mv.output[["s.nlevels"]][2]
+      ) %>% dplyr::select(n, k, everything())}
+    if(nestLevels == 3) {res = res %>%
+      dplyr::mutate(
+        'n' = rma.mv.output[["s.nlevels"]][1],
+        'm' = rma.mv.output[["s.nlevels"]][2],
+        'k' = rma.mv.output[["s.nlevels"]][3]
+      ) %>% dplyr::select(n, m, k, everything())}
   }
   
   # for slope models (with mods) -- ONLY ONE SLOPE
@@ -58,17 +72,17 @@ rma.mv_table <- function(rma.mv.output, special_rowname = "", continuous = FALSE
       stop("This function is meant for models with only one slope (moderator). For models with multiple moderators, please use summary() to ensure you get full information.")
     }
     # conduct data extraction
-    res <- data.frame(
+    res = data.frame(
       'n' = rma.mv.output[["s.nlevels"]][1], 'k' = rma.mv.output[["s.nlevels"]][2],
       'Q' = paste0(round2(rma.mv.output[["QM"]], force = TRUE), sigstars(rma.mv.output[["QMp"]])),
       'b' = round4(rma.mv.output[["b"]][2]), 'SEb' = round4(rma.mv.output[["se"]][2]),
       'CI' = paste0("[", round4(rma.mv.output[["ci.lb"]][2]), ", ", round4(rma.mv.output[["ci.ub"]][2]), "]")
-      )
+    )
   }
   
   # set rowname
-  if(special_rowname == ""){rownames(res) <- NULL}
-  else{rownames(res) <- special_rowname}
+  if(special_rowname == ""){rownames(res) = NULL}
+  else{rownames(res) = special_rowname}
   
   # return formatted row
   return(res)
@@ -76,17 +90,17 @@ rma.mv_table <- function(rma.mv.output, special_rowname = "", continuous = FALSE
 
 
 
-summary_and_forest <- function(metafor.output, xlab = "Effect size", specific_label = "", compressed_summary = FALSE) {
-
+summary_and_forest = function(metafor.output, xlab = "Effect size", specific_label = "", compressed_summary = FALSE) {
+  
   mlab = "Overall effect size"
   if(specific_label != "") {mlab = paste0("Overall effect size for ", specific_label)}
-
+  
   forest(metafor.output, top = 1, xlab = xlab,
          mlab = mlab)
-
-  if(compressed_summary) {out <- rma.mv_table(metafor.output, special_rowname = specific_label)}
-  else {out <- summary(metafor.output)}
-
+  
+  if(compressed_summary) {out = rma.mv_table(metafor.output, special_rowname = specific_label)}
+  else {out = summary(metafor.output)}
+  
   return(out)
 }
 
@@ -94,7 +108,7 @@ summary_and_forest <- function(metafor.output, xlab = "Effect size", specific_la
 
 ##########
 
-endtime <- Sys.time()
+endtime = Sys.time()
 cat("\nFinished loading Nadya's meta-analysis upgrades.")
 cat("\nTime taken:", (endtime - starttime))
 cat("\n####################")
